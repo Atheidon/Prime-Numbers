cebf653be84c9a3ee798a369ae845b6ceff1cffe
Atheidon <93974381+Atheidon@users.noreply.github.com>
2021-11-12 21:02:20 +0100

    Update Prime Number Generator plain.py

--- Prime Number Generator plain.py
+++ Prime Number Generator plain.py
@@ -3,10 +3,10 @@ import time
 x = int(input("Give a number: "))
 def is_prime(n):
     if n <= 1: return False
-    if n == 2:
+    elif n == 2:
         print(2)
         return True
-    if n > 2 and n % 2 == 0: return False
+    elif n > 2 and n % 2 == 0: return False
     max_div = math.floor(math.sqrt(n))
     for i in range(3, 1 + max_div, 2):
         if n % i == 0: return False
