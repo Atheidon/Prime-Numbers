47b2e048b562447a5606802c23a424ec5463393c
Atheidon <93974381+Atheidon@users.noreply.github.com>
2021-11-12 21:00:41 +0100

    Add files via upload

---
+++ Prime Number Checker.py
@@ -0,0 +1,4 @@
+num = int(input("Fill in a number you would like to check (to see if it's a prime number):"))
+if num == 2: print(num,"is a prime number")
+elif num % 2 == 0: print(num,"is not a prime number")
+else: print(num,"is a prime number")
